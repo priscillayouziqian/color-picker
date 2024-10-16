f009a59064d76435be922b9bc938746e284b984d
Zhaoyi Yan <priscillayouziqian@gmail.com>
2024-10-15 22:13:46 -0400

    add features

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const hexInput = document.getElementById('hexInput');
 const inputColor = document.getElementById('inputColor');
+const alteredColor = document.getElementById('alteredColor');
+const alteredColorText = document.getElementById('alteredColorText');
+const sliderText = document.getElementById('sliderText');
+const slider = document.getElementById('slider');
+const lightenText = document.getElementById('lightenText');
+const darkenText = document.getElementById('darkenText');
+const toggleBtn = document.getElementById('toggleBtn');
 
 hexInput.addEventListener('input', displayHex);
+slider.addEventListener('input', displayPercentage);
+toggleBtn.addEventListener('click', displayToggle);
 
 function displayHex() {
     const hex = hexInput.value;
@@ -11,6 +20,7 @@ function displayHex() {
     const strippedHex = hex.replace('#', '');
     //change and display the hex color
     inputColor.style.backgroundColor = "#" + strippedHex;
+    reset();
 }
 
 function convertHexToRGB(hex){
@@ -28,8 +38,16 @@ function convertHexToRGB(hex){
     
     return {r:r, g:g, b:b}
 }
-console.log(convertHexToRGB("ffe"));
+// console.log(convertHexToRGB("ffe"));
+function convertRGBToHex(r,g,b){
+    const firstPair = ("0" + r.toString(16)).slice(-2);
+    const secondPair = ("0" + g.toString(16)).slice(-2);
+    const thirdPair = ("0" + b.toString(16)).slice(-2);
 
+    const hex = "#" + firstPair + secondPair + thirdPair;
+    return hex;
+}
+console.log(convertRGBToHex(0,255,255));
 //check if the color is valid
 function isValidHex (hex){
     // Check if the input hex is falsy (null, undefined, empty string, etc.)
@@ -55,15 +73,63 @@ function isValidHex (hex){
 
 }
 
-console.log(isValidHex('#123456')); //true
-console.log(isValidHex("#0000000")) //false
-console.log(isValidHex("#ffffff")) //true
-console.log(isValidHex("#fff")) //true
-console.log(isValidHex("fff")) //true
-console.log(isValidHex("ac")) //false
-console.log(isValidHex('#ABCDEF'));    // true (valid 6-digit hex)
-console.log(isValidHex('#123GHI'));   // false (invalid character 'G')
+function displayPercentage(){
+    //check hex color if valid.
+    if(!isValidHex(hexInput.value)) return;
+
+    sliderText.textContent = `${slider.value}%`;
+
+    //calculate the appropriate value for the color alteration 
+    //between positive and negative
+    const valueAddition = toggleBtn.classList.contains('toggled') ? -slider.value: slider.value;
+
+    //get the altered hex value
+    const alteredHex = alterColor(hexInput.value, valueAddition);
+
+    //update the altered color
+    alteredColor.style.backgroundColor = alteredHex;
+    alteredColorText.innerText = `Altered Color ${alteredHex}`;
+
+}
+
+function alterColor(hex, percentage){
+    const {r,g,b} = convertHexToRGB(hex); 
+
+    const amount = Math.floor((percentage/100) * 255);
+
+    const newR = increaseWithin0To255(r, amount);
+    const newG = increaseWithin0To255(g, amount);
+    const newB = increaseWithin0To255(b, amount);
+
+    return convertRGBToHex(newR, newG, newB);
+}
+
+function increaseWithin0To255(hex, amount){
+    const newHex = hex + amount;
+    if(newHex > 255) return 255;
+    if(newHex < 0) return 0;
+    return newHex;
+}
 
 
 
+function displayToggle(){
+    if(toggleBtn.classList.contains('toggled')){
+        toggleBtn.classList.remove('toggled');
+        lightenText.classList.remove('unselected');
+        darkenText.classList.add('unselected');
+    }else{
+        toggleBtn.classList.add('toggled');
+        lightenText.classList.add('unselected');
+        darkenText.classList.remove('unselected');
+    }
+    reset();
+}
+
+function reset(){
+    slider.value = 0;
+    sliderText.innerText = `0%`;
+    alteredColor.style.backgroundColor = hexInput.value;
+    alteredColorText.innerText = `Altered Color ${hexInput.value}`;
+}
 
